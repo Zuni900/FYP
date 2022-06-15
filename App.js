4f17419473e4a5e4a240930875462a2231783bd9
Zunaira <zunairaasif900@gmail.com>
2022-06-16 02:04:53 +0500

    ....

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React, {useState} from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import NavigationTheme from "./app/navigation/NavigationTheme";
 import {RootNavigator} from "./app/navigation/rootNavigator";
-import { UserContext } from "./app/context/Index";
+import {UserContext} from "./app/context/Index";
 
 export default function App() {
 
-  const[userEmail,setEmail]=useState();
+  const[userEmail, setEmail] = useState();
+
   return (
-    <UserContext.Provider value={{userEmail,setEmail}}>
+    <UserContext.Provider value = {{userEmail, setEmail}}>
       <NavigationContainer theme = {NavigationTheme}>
         <RootNavigator />
       </NavigationContainer>
